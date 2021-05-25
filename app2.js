3c41a9dccac60215db7883270993330b47ca5cde
Yoel31 <yoel301@gmail.com>
2021-05-25 14:06:17 +0300

    dfd

--- app2.js
+++ app2.js
@@ -11,7 +11,6 @@ require('dotenv').config();
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // app.use(logger('dev'));
